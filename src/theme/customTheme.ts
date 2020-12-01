8994b7048b396ad8dd226f828152ef85075cd3de
domuk-k <dannyworks102@gmail.com>
2020-12-02 00:06:51 +0900

    theme: update theme configuration

--- src/theme/customTheme.ts
+++ src/theme/customTheme.ts
@@ -9,11 +9,16 @@ const customTheme = extendTheme({
       h1: {
         fontSize: '1.8rem',
       },
+      ul: {
+        listStyle: 'none',
+      },
     }),
   },
   colors: {
     brand: 'rgb(121, 75, 196)',
+    brandAlpha: 'rgba(121, 75, 196,.3)',
     background: {
+      light: 'rgb(235, 238, 240)',
       100: 'rgb(61, 84, 102)',
       200: 'rgb(56, 68, 77)',
       300: 'rgb(21,32,43)',
