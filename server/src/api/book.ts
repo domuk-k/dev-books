e2d829b2b3a281b04a21ccac59515937f502db9b
domuk-k <dannyworks102@gmail.com>
2020-12-16 14:13:10 +0900

    chore: departed server code

---
+++ server/src/api/book.ts
@@ -0,0 +1,56 @@
+import express, { NextFunction, Request, Response } from 'express';
+import BookModel, { BookDocument } from '../model/Book';
+import DB from '../utils/db';
+
+const router = express.Router();
+const db = new DB<BookDocument>(BookModel);
+
+// READ ALL
+router.get('/', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const data = await db.read({});
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    next();
+  }
+});
+
+// READ USER'S BOOKLIST
+
+// CREATE ONE
+router.post('/', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const data = await db.create(req.body);
+    res.json(data);
+  } catch (error) {
+    res.json({ error });
+    next();
+  }
+});
+
+// UPDATE ONE
+router.patch('/', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const data = await db.update(req.body);
+    res.json(data);
+  } catch (error) {
+    res.json({ error });
+    next();
+  }
+});
+
+// DELETE ONE
+router.delete(
+  '/:id',
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.json(await db.delete(req.params.id));
+    } catch (error) {
+      res.json({ error: new Error(error) });
+      next();
+    }
+  }
+);
+
+export default router;
